62c141eb9582c4c1bd092d2e1f69b5a9f3187071
Erik de Souza Scheffer <erikscheffer@yahoo.com>
2020-04-20 11:21:55 -0300

    Correction to CORS

--- deliver-it-test-backend/src/main/java/com/example/erikscheffer/deliverittestbackend/resource/BillResource.java
+++ deliver-it-test-backend/src/main/java/com/example/erikscheffer/deliverittestbackend/resource/BillResource.java
@@ -2,18 +2,17 @@ package com.example.erikscheffer.deliverittestbackend.resource;
 
 import com.example.erikscheffer.deliverittestbackend.model.Bill;
 import com.example.erikscheffer.deliverittestbackend.model.api.BillApiCreate;
-import com.example.erikscheffer.deliverittestbackend.repository.BillRepository;
 import com.example.erikscheffer.deliverittestbackend.service.BillService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/bill")
+@CrossOrigin
 public class BillResource {
 
     private final BillService billService;
